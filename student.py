511881ca6198f1468e5d7e3187b7de9fb4b941dd
barnabaserdelyi <derdelyi7@gmail.com>
2016-12-14 13:02:37 +0100

    first round

--- student.py
+++ student.py
@@ -2,4 +2,16 @@ from person import Person
 
 
 class Student(Person):
-    pass
+    def __init__(self, first_name, last_name, year_of_birth, gender, knowledge_level = 0):
+        super.__init__(self, first_name, last_name, year_of_birth, gender)
+        self.knowledge_level = knowledge_level
+
+    @staticmethod
+    def create_by_csv(csv_file):
+        with open(csv_file, "r") as data:
+            students = csv.reader(data)
+            list_of_students = [Student(row[0], row[1], row[2], row[3], row[4]) for row in students]
+        return list_of_students
+    
+    def knowledge_level_charger(amount):
+        return self.knowledge_level += amount
