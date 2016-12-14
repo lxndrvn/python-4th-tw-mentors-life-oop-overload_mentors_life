b2f5d1432608937faef84a38245589179fffd8be
olajx <perfanni@gmail.com>
2016-12-14 13:36:01 +0100

    update_fani

--- mentor.py
+++ mentor.py
@@ -1,5 +1,22 @@
 from person import Person
+import csv
 
 
 class Mentor(Person):
-    pass
+
+    def __init__(self, fullname, soft_skill_level):
+        super().__init__(*args)
+        self.fullname = fullname
+        self.soft_skill_level = int(soft_skill)
+
+    def soft_skill_level_changer(self, charger):
+        self.nicotine_level += int(delta)
+        return self.nicotine_level
+
+    def create_by_csv(cls, file_name):
+        mentor_list = []
+        with open(file_name, "r") as f:
+            reader = csv.reader(f, delimiter=",")
+            for i in reader:
+                mentor_list.append(Mentor(i[0], i[1], i[2], i[3], i[4], i[5], i[6]))
+        return mentor_list
--- person.py
+++ person.py
@@ -1,2 +1,37 @@
+import random
+
+
 class Person:
-    pass
+    Gender = ['male', 'female', 'not sure']
+
+    def __init__(self, first_name=None, last_name=None,
+                 year_of_birth=None, gender=None, energy_level, happiness_level, nicotine_level):
+        if gender not in self.Gender:
+            raise ValueError('%s is not a valid gender.' % gender)
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.year_of_birth = int(year_of_birth)
+        self.gender = gender
+        self.energy_level = int(energy_level)
+        self.happiness_level = int(happiness_level)
+
+    def energy_level_changer(self, delta):
+        self.energy_level += int(delta)
+        return self.energy_level
+
+    def happiness_level_changer(self, delta):
+        self.happiness_level += int(delta)
+        return self.happiness_level
+
+    def nicotine_level_changer(self, delta):
+        self.nicotine_level += int(delta)
+        return self.nicotine_level
+
+    def nicotine_level_charger(self):
+        if self.energy_level < 100:
+            self.energy_level += 25
+        if self.happiness_level < 100:
+            self.energy_level += 20
+        if self.nicotine_level < 100:
+            self.nicotine_level += 50
