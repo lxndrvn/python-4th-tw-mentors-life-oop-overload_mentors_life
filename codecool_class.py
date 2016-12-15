dd47b86754183bb0348290d276ce45a9126e3a0f
lxndrvn <i1990522@gmail.com>
2016-12-15 11:15:54 +0100

    code added 1.0

--- codecool_class.py
+++ codecool_class.py
@@ -1,6 +1,15 @@
 from mentor import Mentor
 from student import Student
 
-
 class CodecoolClass:
-    pass
+    def __init__(self, location):
+        self.location = location
+
+    @classmethod
+    def generate_local():
+        location = "Budapest"
+        year = 2016
+        mentors = create_
+        students = create_
+        return CodecoolClass(location)
+
